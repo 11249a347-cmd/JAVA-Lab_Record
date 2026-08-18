d600caf8f605c30ae46be03e6787cbe14e2ad401
Amrutha Shivapuram <11249a347@kanchiuniv.ac.in>
2026-08-18 11:45:26 +0530

    Add files via upload

---
+++ HierarchialInheritance.java
@@ -0,0 +1,28 @@
+class Animal {
+void eat() {
+System.out.println("eating...");
+}
+}
+
+class Dog extends Animal{
+void bark() {
+System.out.println("Barking....");
+}
+}
+
+class Cat extends Animal{
+void meow() {
+System.out.println("Meowing...");
+}
+}
+
+class HierarchialInheritance {
+public static void main (String[] args) {
+Cat c = new Cat();
+c.meow();
+c.eat();
+Dog d = new Dog();
+d.bark();
+d.eat();
+}
+}
---
+++ Interface1.java
@@ -0,0 +1,23 @@
+interface Drawable{
+void draw();
+}
+
+class Rectangle implements Drawable {
+public void draw()
+{
+System.out.println("Drawing Rectangle");
+}
+}
+
+class Circle implements Drawable{
+public void draw() {
+System.out.println("Drawing Circle");
+}
+}
+
+public class Interface1{
+public static void main(String[] args) {
+Drawable d = new Circle();
+d.draw();
+}
+}
